2288c8c73cf7fc8bb7f0693119b739a77e25cfc8
Alessandro Lentini <alelentini@live.com>
2019-07-04 20:38:27 +0200

    Udder Project added in GitHub

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Alessandro Lentini on 04/07/2019.
+//  Copyright © 2019 Alessandro Lentini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
